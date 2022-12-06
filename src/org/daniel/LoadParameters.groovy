d3889df5970ce6245b12207d83e2256f13365e61
Hardikrathod01 <42797234+Hardikrathod01@users.noreply.github.com>
2022-12-06 16:15:13 +0100

    Update LoadParameters.groovy

--- src/org/daniel/LoadParameters.groovy
+++ src/org/daniel/LoadParameters.groovy
@@ -11,12 +11,7 @@ class LoadParameters implements Serializable {
   
   def load(param) {
     steps.bat "echo load parameters from jenkins: ${param}"
-//     steps.properties([
-//       steps.parameters {
-//         ChoiceParameterDefinition("TestChoice", ["choice1", "choice2"], "this is test choice parameter")
-//       }
-//     ])
-    steps.properties([steps.parameters([steps.string(defaultValue: 'test', description: 'test', name: 'test')])])
+    steps.properties([steps.parameters([steps.string(defaultValue: 'test', description: 'test', name: 'test'), steps.choice(choices: ['choice1', 'choice2'], description: 'this is test choice parameter', name: 'ChoiceParam')])])
   }
   
 }
