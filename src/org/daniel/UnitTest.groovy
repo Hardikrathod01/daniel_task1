ce8e795ff5118c82069bc5cbeb32286e1773bded
Hardikrathod01 <42797234+Hardikrathod01@users.noreply.github.com>
2022-12-06 17:35:57 +0100

    Update UnitTest.groovy

--- src/org/daniel/UnitTest.groovy
+++ src/org/daniel/UnitTest.groovy
@@ -2,16 +2,15 @@ package org.daniel
 
 import hudson.model.ChoiceParameterDefinition
 
-class LoadParameters implements Serializable {
+class UnitTest implements Serializable {
   def steps
   
-  LoadParameters(steps) {
+  UnitTest(steps) {
     this.steps = steps
   }
   
-  def load(param) {
-    steps.bat "echo load parameters from jenkins: ${param}"
-    steps.properties([steps.parameters([steps.string(defaultValue: 'test', description: 'test', name: 'test'), steps.choice(choices: ['choice1', 'choice2'], description: 'this is test choice parameter', name: 'ChoiceParam')])])
+  def runTests() {
+    steps.bat "echo this is unit test stage"
   }
   
 }
