902549ab4f445b80d1200cfd3695554f56a202ff
Hardikrathod01 <42797234+Hardikrathod01@users.noreply.github.com>
2022-12-06 17:33:04 +0100

    Update ApprovalInput.groovy

--- src/org/daniel/ApprovalInput.groovy
+++ src/org/daniel/ApprovalInput.groovy
@@ -2,16 +2,15 @@ package org.daniel
 
 import hudson.model.ChoiceParameterDefinition
 
-class LoadParameters implements Serializable {
+class ApprovalInput implements Serializable {
   def steps
   
-  LoadParameters(steps) {
+  ApprovalInput(steps) {
     this.steps = steps
   }
   
-  def load(param) {
-    steps.bat "echo load parameters from jenkins: ${param}"
-    steps.properties([steps.parameters([steps.string(defaultValue: 'test', description: 'test', name: 'test'), steps.choice(choices: ['choice1', 'choice2'], description: 'this is test choice parameter', name: 'ChoiceParam')])])
+  def takeInput() {
+    steps.input message: 'Please decide whether pipeline should move forward or not', ok: 'Continue Build?'
   }
   
 }
